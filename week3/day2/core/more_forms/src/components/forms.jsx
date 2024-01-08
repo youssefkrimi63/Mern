55794fd4a7f28d6b5245eec54d2f08d1572f75a5
youssefkrimi63 <krimiyoussef63@gmail.com>
2024-01-08 21:30:19 +0100

    Mern

--- week3/day2/core/more_forms/src/components/forms.jsx
+++ week3/day2/core/more_forms/src/components/forms.jsx
@@ -12,7 +12,6 @@ const UserForm = () => {
         const newUser = { firstName, lastName, email, password, confirmPassword };
         console.log("Welcome", newUser);
 
-        
         setFirstName("");
         setLastName("");
         setEmail("");
@@ -21,62 +20,68 @@ const UserForm = () => {
     };
 
     const InputMessage = () => {
-        if ( firstName.length < 2) {
+        if (firstName.length > 0 && firstName.length < 2) {
             return "First Name must be at least 2 characters";
         }
         return null;
     };
 
+    const LastNameMessage = () => {
+        if (lastName.length > 0 && lastName.length < 2) {
+            return "Last Name must be at least 2 characters";
+        }
+        return null;
+    };
+
+    const EmailMessage = () => {
+        if (email.length > 0 && email.length < 2 ) {
+            return "Invalid email must be at least 2 characters";
+        }
+        return null;
+    };
+
     const PasswordMessage = () => {
-        if ( password.length < 8) {
+        if (password.length > 0 && password.length < 8) {
             return "Password must be at least 8 characters";
         }
         return null;
     };
 
     const ConPasswordMessage = () => {
-        if (password !== confirmPassword) {
+        if (confirmPassword.length > 0 && password !== confirmPassword) {
             return "Passwords must match";
         }
         return null;
     };
 
-
-    
-    
     return (
         <form onSubmit={createUser}>
             <div>
                 <label>Prénom: </label> 
                 <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                <h3>{InputMessage}</h3>
+                <h3>{InputMessage()}</h3>
             </div>
             <div>
                 <label>Nom: </label> 
                 <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                <h3>{InputMessage}</h3>
+                <h3>{LastNameMessage()}</h3>
             </div>
-
             <div>
                 <label>Adresse e-mail: </label> 
                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <h3>{InputMessage}</h3>
+                <h3>{EmailMessage()}</h3>
             </div>
             <div>
                 <label>Mot de passe: </label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <h3>{PasswordMessage}</h3>
+                <h3>{PasswordMessage()}</h3>
             </div>
             <div>
-
                 <label>Confirmer le mot de passe: </label>
                 <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                <h3>{ConPasswordMessage}</h3>
+                <h3>{ConPasswordMessage()}</h3>
             </div>
-            
-
-           
-        
+          
         </form>
     );
 };
